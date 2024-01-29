39c3b0105ef6721fd414807e0f86776639acd459
dav-dmdo <david.davila@correo.unimet.edu.ve>
2024-01-28 22:51:30 -0400

    new class: :beers: Worker class

---
+++ src/Classes/Worker.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author david
+ */
+public class Worker extends Thread{
+    private int type;
+    private int productionPerDay;
+    private int dayDuration;
+    private int account;
+    
+    @Override
+    public void run(){
+        
+    }
+    
+}
